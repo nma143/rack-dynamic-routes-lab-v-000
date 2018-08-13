4c7c71d09080ecdff829c5348e30b2d51f8d1418
Nikki A <nikki.apostolakis@gmail.com>
2018-08-13 17:54:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("items").last
-      item = @@items.find(|i| i.name == item_name)
+      item = @@items.find{|i| i.name == item_name}
       if item
         resp.write "Price is #{item.price}"
       else
