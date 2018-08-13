9710d102874a70dcdc6dd2245a4e82e12542fd1d
Nikki A <nikki.apostolakis@gmail.com>
2018-08-13 17:54:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
         resp.status = 400
       end
     else
-      resp.write "Path Not Found"
+      resp.write "Route not found"
       resp.status = 404
     end
     resp.finish
