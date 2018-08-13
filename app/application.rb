6ad2a58bc544f264e9b4adb1883a1122374da25f
Nikki A <nikki.apostolakis@gmail.com>
2018-08-13 18:00:30 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("items").last
       item = @@items.find{|i| i.name == item_name}
+      resp.write "Hey nikki: #{item}"
       if item
         resp.write "Price is #{item.price}"
       else
